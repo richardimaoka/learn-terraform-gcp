e6784ea9a50f807951794fbc7f99368ade938627
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-14 09:09:22 +0900

    cleaned up

--- main.tf
+++ main.tf
@@ -1,25 +1,20 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
     }
   }
 }
 
-provider "docker" {}
+provider "google" {
+  credentials = file("<NAME>.json")
 
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+  project = "<PROJECT_ID>"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
 }
-
