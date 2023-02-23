3292002bb18847a44c0f6269b0e81ba231e22241
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-02-23 17:04:12 +0900

    https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build

--- main.tf
+++ main.tf
@@ -1,17 +1,20 @@
-resource "google_cloud_run_service" "default" {
-  name     = "cloudrun-srv"
-  location = "us-central1"
-
-  template {
-    spec {
-      containers {
-        image = "us-docker.pkg.dev/cloudrun/container/hello"
-      }
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.54.0"
     }
   }
+}
 
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
+provider "google" {
+  credentials = file("<NAME>.json")
+
+  project = "<PROJECT_ID>"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
 }
